0aa8b1de00d0ff0f5562c3fab7d7188b797492da
williamjpriest <liuliu6wp@gmail.com>
2024-02-24 13:32:23 -0500

    Feat : Accepts user input

--- app.js
+++ app.js
@@ -1,19 +1,24 @@
 import OpenAI from "openai";
 import 'dotenv/config'
+import readline from 'readline';
 
 const openai = new OpenAI(
 );
 
 async function main() {
-    
-    let userRes = "I need help with my math homework"
-    const completion = await openai.chat.completions.create({
+    const rl = readline.createInterface({
+        input: process.stdin,
+        output: process.stdout
+      });
+    rl.question('How may I help?', async (userRes) => {
+        const completion = await openai.chat.completions.create({
         messages: [{ role: "system", content: "You are a helpful assistant."},{role: "user",content:userRes}],
 
         model: "gpt-3.5-turbo",
     });
-
-    console.log(completion.choices[0]);
+        rl.close();
+        console.log(completion.choices[0]);
+    });
+    
 }   
-
 main();
