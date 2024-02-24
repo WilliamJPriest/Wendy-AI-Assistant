b11995a9809801ef4108a37592b68321626c7e74
williamjpriest <liuliu6wp@gmail.com>
2024-02-24 13:04:26 -0500

    feat - can have dynamic queries

--- app.js
+++ app.js
@@ -5,12 +5,15 @@ const openai = new OpenAI(
 );
 
 async function main() {
-  const completion = await openai.chat.completions.create({
-    messages: [{ role: "system", content: "You are a helpful assistant." }],
-    model: "gpt-3.5-turbo",
-  });
+    
+    let userRes = "I need help with my math homework"
+    const completion = await openai.chat.completions.create({
+        messages: [{ role: "system", content: "You are a helpful assistant."},{role: "user",content:userRes}],
 
-  console.log(completion.choices[0]);
-}
+        model: "gpt-3.5-turbo",
+    });
+
+    console.log(completion.choices[0]);
+}   
 
 main();
