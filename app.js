d747cfe134732316dd1f98441074d54859d9ec26
williamjpriest <liuliu6wp@gmail.com>
2024-02-29 15:11:37 -0500

    Refactor: cleaned up code and rmf unneeded npm packages

--- app.js
+++ app.js
@@ -5,9 +5,9 @@ import path from 'path';
 import { OpenAI } from 'openai';
 import Player from 'play-sound';
 
+
 const openai = new OpenAI();
 
-// Configure play-sound to use the default system player
 const player = new Player();
 
 const speechFile = path.resolve('./speech.mp3');
@@ -15,47 +15,39 @@ const speechFile = path.resolve('./speech.mp3');
 async function main() {
 
     const transcription = await openai.audio.transcriptions.create({
-        file: fs.createReadStream("audio.mp3"),
+        file: fs.createReadStream("Rev.mp3"),
         model: "whisper-1",
       });
 
-      userRes = transcription.text
+    let userRes = transcription.text
 
-    const rl = readline.createInterface({
-        input: process.stdin,
-        output: process.stdout,
+
+
+    const completion = await openai.chat.completions.create({
+        messages: [
+            { role: 'system', content: 'You are a helpful assistant named Wendy ' },
+            { role: 'user', content: userRes },
+        ],
+        model: 'gpt-3.5-turbo',
     });
 
-    rl.question('How may I help? ', async (userRes) => {
-        const completion = await openai.chat.completions.create({
-            messages: [
-                { role: 'system', content: 'You are a helpful assistant named Wendy ' },
-                { role: 'user', content: userRes },
-            ],
-            model: 'gpt-3.5-turbo',
-        });
 
-        rl.close();
-        console.log(completion.choices[0]);
-
-        const mp3 = await openai.audio.speech.create({
-            model: 'tts-1',
-            voice: 'nova',
-            input: completion.choices[0].message.content,
-        });
-
-        console.log(speechFile);
-
-        const buffer = Buffer.from(await mp3.arrayBuffer());
-        await fs.promises.writeFile(speechFile, buffer);
-
-
-        player.play(speechFile, (err) => {
-            if (err) {
-                console.error('Error playing audio:', err);
-            }
-        });
+    const mp3 = await openai.audio.speech.create({
+        model: 'tts-1',
+        voice: 'nova',
+        input: completion.choices[0].message.content,
     });
-}
+
+
+    const buffer = Buffer.from(await mp3.arrayBuffer());
+    await fs.promises.writeFile(speechFile, buffer);
+
+
+    player.play(speechFile, (err) => {
+        if (err) {
+            console.error('Error playing audio:', err);
+        }
+    });
+};
 
 main();
